6aadf3ced465978cfe4241d8fe5aeac977640f68
aman1862000 <rjamans18@gmail.com>
2022-03-25 15:29:41 +0530

    Add project files.

---
+++ HangFire/Book.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangFire
+{
+    public class Book
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public string Description { get; set; }
+        public string Thumbnail { get; set; }
+        public string Price { get; set; }
+    }
+
+    public class BookPurchaseVM : Book
+    {
+        public string Nonce { get; set; }
+    }
+}
---
+++ HangFire/Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using Braintree;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HangFire.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        private readonly IBraintreeService _braintreeService;
+        public HomeController(IBraintreeService braintreeService)
+        {
+            _braintreeService = braintreeService;
+        }
+        public IActionResult Index()
+        {
+            var gateway = _braintreeService.GetGateway();
+            var clientToken = gateway.ClientToken.Generate();  //Genarate a token
+            //ViewBag.ClientToken = clientToken;
+
+            var data = new BookPurchaseVM
+            {
+                Id = 2,
+                Description = "Hellow man",
+                Author = "Me",
+                Thumbnail = "This is thumbnail",
+                Title = "This is title",
+                Price = "230",
+                Nonce = ""
+            };
+
+            return Ok(new { data,clientToken});
+        }
+        [HttpPost]
+        public IActionResult Create(BookPurchaseVM model)
+        {
+            var gateway = _braintreeService.GetGateway();
+            var request = new TransactionRequest
+            {
+                Amount = Convert.ToDecimal("250"),
+                PaymentMethodNonce = model.Nonce,
+                Options = new TransactionOptionsRequest
+                {
+                    SubmitForSettlement = true
+                }
+            };
+
+            Result<Transaction> result = gateway.Transaction.Sale(request);
+
+            if (result.IsSuccess())
+            {
+                return Ok("Success");
+            }
+            else
+            {
+                return Ok("Failure");
+            }
+        }
+
+    }
+}
